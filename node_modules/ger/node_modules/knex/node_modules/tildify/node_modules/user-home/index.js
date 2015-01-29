23d4afb2bc3b8386b0b6bcb7e4dd6f2e1d3f47d5
Graham Jenson <grahamjenson@maori.geek.nz>
2015-01-29 21:18:04 +1300

    updated ger

--- node_modules/ger/node_modules/knex/node_modules/tildify/node_modules/user-home/index.js
+++ node_modules/ger/node_modules/knex/node_modules/tildify/node_modules/user-home/index.js
@@ -4,11 +4,12 @@ var home = env.HOME;
 var user = env.LOGNAME || env.USER || env.LNAME || env.USERNAME;
 
 if (process.platform === 'win32') {
-	module.exports = env.USERPROFILE || env.HOMEDRIVE + env.HOMEPATH || home;
+	module.exports = env.USERPROFILE || env.HOMEDRIVE + env.HOMEPATH || home || null;
 } else if (process.platform === 'darwin') {
-	module.exports = home || (user ? '/Users/' + user : null);
+	module.exports = home || (user ? '/Users/' + user : null) || null;
 } else if (process.platform === 'linux') {
-	module.exports = home || (user ? (process.getuid() === 0 ? '/root' : '/home/' + user) : null);
+	module.exports = home ||
+		(user ? (process.getuid() === 0 ? '/root' : '/home/' + user) : null) || null;
 } else {
-	module.exports = home;
+	module.exports = home || null;
 }
